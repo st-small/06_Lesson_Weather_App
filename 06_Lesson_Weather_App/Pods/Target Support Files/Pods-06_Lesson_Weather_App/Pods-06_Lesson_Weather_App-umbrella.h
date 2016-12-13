12f06a11777e4a018cb5c91a75ef56d24d8feea3
Stanly Shiyanovskiy <st-small@rambler.ru>
2016-12-13 21:02:45 +0200

    added pods

---
+++ 06_Lesson_Weather_App/Pods/Target Support Files/Pods-06_Lesson_Weather_App/Pods-06_Lesson_Weather_App-umbrella.h
@@ -0,0 +1,8 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#endif
+
+
+FOUNDATION_EXPORT double Pods_06_Lesson_Weather_AppVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_06_Lesson_Weather_AppVersionString[];
+
