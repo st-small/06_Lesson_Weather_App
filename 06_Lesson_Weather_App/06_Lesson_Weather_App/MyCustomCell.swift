3b373fb0b7a6753b4832bd0b2e3d07c7999f8aea
Stanly Shiyanovskiy <st-small@rambler.ru>
2016-12-13 22:06:02 +0200

    создал SelectCityViewController

---
+++ 06_Lesson_Weather_App/06_Lesson_Weather_App/MyCustomCell.swift
@@ -0,0 +1,17 @@
+//
+//  MyCustomCell.swift
+//  06_Lesson_Weather_App
+//
+//  Created by Stanly Shiyanovskiy on 13.12.16.
+//  Copyright © 2016 Stanly Shiyanovskiy. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class MyCustomCell: UITableViewCell {
+    
+    @IBOutlet weak var myView: UIImageView!
+    @IBOutlet weak var myCellLabel: UILabel!
+    
+}
---
+++ 06_Lesson_Weather_App/06_Lesson_Weather_App/SelectCityViewController.swift
@@ -0,0 +1,61 @@
+//
+//  SelectCityViewController.swift
+//  06_Lesson_Weather_App
+//
+//  Created by Stanly Shiyanovskiy on 13.12.16.
+//  Copyright © 2016 Stanly Shiyanovskiy. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+final class SelectCityViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    @IBOutlet var tableView: UITableView!
+    var citiesArray: [String] = ["Kiev", "Moscow", "London"]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.navigationItem.title = "Выбор города:"
+        
+        // delegate and data source
+        tableView.delegate = self
+        tableView.dataSource = self
+    }
+    
+    // number of rows in table view
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return citiesArray.count
+    }
+    
+    // size of row's height
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 77.0
+    }
+    
+    // create a cell for each table view row
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell: MyCustomCell = self.tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath as IndexPath) as! MyCustomCell
+        cell.myCellLabel?.text = "\(citiesArray[indexPath.row])"
+        return cell
+    }
+    
+    // method to run when table view cell is tapped
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print("You tapped cell number \(indexPath.row).")
+    }
+    
+    // methods to delete rows
+    func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        return true
+    }
+    
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            citiesArray.remove(at: indexPath.row)
+            tableView.reloadData()
+        }
+    }
+}
